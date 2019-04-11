a1c6902e844f97432954292ccabe9870e39fde3f
Marshal Ma <mashuo93@gmail.com>
2019-04-10 22:09:12 -0700

    add common cpp header file for src files.

---
+++ include/common.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
+#include <stack>
+#include <deque>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <cmath>
+#include <climits>
+#include <unordered_set>
+
+using namespace std;
